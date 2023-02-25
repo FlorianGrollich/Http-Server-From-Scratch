35a50eff5530ae2498e82c08fe498d5797ce4a8f
FlorianGrollich <F-Grollich@web.de>
2023-02-25 11:15:35 +0100

    add socket

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <ws2spi.h>
+
 
 int main() {
-    printf("Hello, World!\n");
+    int server_fd;
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+
+    }
+
     return 0;
+
 }
