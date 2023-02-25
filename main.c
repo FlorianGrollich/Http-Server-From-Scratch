4187423cb2d99fdbf91ad084fc16a4d73203b212
FlorianGrollich <F-Grollich@web.de>
2023-02-25 14:03:52 +0100

    add socket

--- main.c
+++ main.c
@@ -7,7 +7,18 @@
 int main() {
     int server_fd;
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("cannot create socket");
+        return 0;
+    }
 
+    struct sockaddr_in address;
+    const int PORT = 8080;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    address.sin_port = htons(PORT); if (bind(server_fd,(struct sockaddr *)&address,sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        return 0;
     }
 
     return 0;
