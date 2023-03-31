7a4957b24c9315cc073e41858a185b1eb785044e
FlorianGrollich <F-Grollich@web.de>
2023-03-31 20:38:21 +0200

    add httpmethods enum

---
+++ server/HTTPRequest.h
@@ -0,0 +1,17 @@
+
+
+
+#ifndef HTTPRequest_h
+#define HTTPRequest_h
+
+enum HTTPMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    HEAD,
+    OPTIONS,
+    PATCH
+};
+
+#endif /* HTTPRequest_h */
