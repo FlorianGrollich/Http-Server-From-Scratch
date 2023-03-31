2834865ad7cc4432e0baffa2551aaf273116a569
FlorianGrollich <F-Grollich@web.de>
2023-03-31 21:03:16 +0200

    asd

--- server/HTTPRequest.c
+++ server/HTTPRequest.c
@@ -6,7 +6,9 @@
 
 struct HTTPRequest http_request_constructor(char *request_string) {
     for (int i = 0; i< strlen(request_string)-2; i++) {
-
+        if(request_string[i] == '\n' && request_string[i+1] == '\n') {
+            request_string[i+1] = '|';
+        }
     }
     char *request_line = strtok(request_string, "\n");
 
